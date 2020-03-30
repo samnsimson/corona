e714bf7dabf8ad82912bd6f1fd3e53740f5db928
Sam1996 <samnishanth01@gmail.com>
2020-03-31 02:28:33 +0530

    Option to sort table by value

--- src/components/CaseDataInTable.js
+++ src/components/CaseDataInTable.js
@@ -9,6 +9,8 @@ export default class CaseDataInTable extends React.Component {
 		this.state = {
 			tableData: [],
 			loading: true,
+			ASC: false,
+			ddefaultSortKey: "activecases",
 		}
 	}
 
@@ -16,7 +18,7 @@ export default class CaseDataInTable extends React.Component {
 		this.fetchdata().then(result => {
 			this.setState(
 				{
-					tableData: result.data,
+					tableData: this.sortData(result.data, "activecases", this.state.ASC),
 				},
 				() => {
 					this.setState({
@@ -24,6 +26,19 @@ export default class CaseDataInTable extends React.Component {
 					})
 				}
 			)
+		})
+	}
+
+	sortData = (array, key, asc) => {
+		return asc
+			? array.sort((a, b) => a[key] - b[key])
+			: array.sort((a, b) => b[key] - a[key])
+	}
+
+	toggleSortOrder = key => {
+		this.setState({
+			tableData: this.sortData(this.state.tableData, key, !this.state.ASC),
+			ASC: !this.state.ASC,
 		})
 	}
 
@@ -38,13 +53,31 @@ export default class CaseDataInTable extends React.Component {
 	render() {
 		return (
 			<div>
+				<span style={{ fontSize: "12px", color: "#d3d3d3" }}>
+					Click on A, C or D to sort
+				</span>
 				<Table borderless style={{ fontSize: "14px" }}>
 					<thead style={{ background: "#DBEEFB" }}>
 						<tr>
 							<th>STATE/UT</th>
-							<th>A</th>
-							<th>C</th>
-							<th>D</th>
+							<th
+								style={{ cursor: "pointer" }}
+								onClick={() => this.toggleSortOrder("activecases")}
+							>
+								A
+							</th>
+							<th
+								style={{ cursor: "pointer" }}
+								onClick={() => this.toggleSortOrder("curedcases")}
+							>
+								C
+							</th>
+							<th
+								style={{ cursor: "pointer" }}
+								onClick={() => this.toggleSortOrder("deadcases")}
+							>
+								D
+							</th>
 						</tr>
 					</thead>
 					<tbody style={{ background: "#F3F9FE" }}>
@@ -70,6 +103,22 @@ export default class CaseDataInTable extends React.Component {
 						)}
 					</tbody>
 				</Table>
+				<div>
+					<span
+						className="d-flex justify-content-between"
+						style={{ fontSize: "12px" }}
+					>
+						<span className="text-primary">
+							<b>A</b> - Active
+						</span>
+						<span className="text-success">
+							<b>B</b> - Cured
+						</span>
+						<span className="text-danger">
+							<b>D</b> - Dead
+						</span>
+					</span>
+				</div>
 			</div>
 		)
 	}
